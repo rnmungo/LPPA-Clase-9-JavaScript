b0f2b0fe9866d08337f056c4e3588420ed3508df
Rodrigo Mungo <rodrigomungo@gmail.com>
2022-05-30 23:34:11 -0300

    Resolve exercise of variables and operators

---
+++ exercises/variables_and_operators.js
@@ -0,0 +1,17 @@
+// Exercise A
+const firstNumber = 10;
+const secondNumber = 20;
+const thirdNumber = firstNumber + secondNumber;
+console.log(thirdNumber);
+
+// Exercise B
+const firstString = 'Hello';
+const secondString = 'World';
+const thirdString = firstString + secondString;
+console.log(thirdString);
+
+// Exercise C
+const firstLength = 'Hello';
+const secondLength = 'World';
+const thirdLengthNumber = firstLength.length + secondLength.length;
+console.log(thirdLengthNumber);
